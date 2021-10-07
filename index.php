1a4ea031db3996ccb8bf28efc5edc1314ff81361
StanCheezy <stanlycheezy@gmail.com>
2021-10-07 15:55:44 +0200

    PHP

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        $p = "Here i come from the valley of survival and strong holdship"
+        
+        ?>
+    </body>
+</html>
